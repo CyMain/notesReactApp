ab7a468eb2b2b4311c7f936dee0bb0e5e0997f5b
Cyrus <okikiolacyrus@gmail.com>
2025-06-29 19:17:39 +0100

    Notes Layout completed

--- src/Notes/Notes.jsx
+++ src/Notes/Notes.jsx
@@ -1,11 +1,11 @@
-import React, {useState, useEffect, useRef, useContext} from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import { nameContext } from "../MainComponent/MainComponent.jsx";
 import './notes.css'
 
-function Notes(){
-    const {name, editView, notesView, headerRef, notes, setNotes, currNotes, setCurrNotes, currFilter, setCurrFilter, isEditView, setIsEditView, editingNoteId, setEditingNoteId, editTitle, setEditTitle, editContent, setEditContent, date, groups, setGroups, createNoteAndEdit} = useContext(nameContext);
+function Notes() {
+    const { name, editView, notesView, headerRef, notes, setNotes, currNotes, setCurrNotes, currFilter, setCurrFilter, isEditView, setIsEditView, editingNoteId, setEditingNoteId, editTitle, setEditTitle, editContent, setEditContent, date, groups, setGroups, createNoteAndEdit } = useContext(nameContext);
 
-    useEffect(()=>{
+    useEffect(() => {
         localStorage.setItem("storedNotes", JSON.stringify(notes))
         setCurrNotes([...notes]);
     }, [notes]);
@@ -18,43 +18,43 @@ function Notes(){
         }
     }, [headerRef]);
 
-    function getDateString(year, month, day, hours, mins){
-        if(date.getFullYear() == year){
-            if(date.getMonth() == month){
-                if(date.getDate() == day){
-                    if(date.getHours()== hours){
-                        if(date.getMinutes()==mins){
+    function getDateString(year, month, day, hours, mins) {
+        if (date.getFullYear() == year) {
+            if (date.getMonth() == month) {
+                if (date.getDate() == day) {
+                    if (date.getHours() == hours) {
+                        if (date.getMinutes() == mins) {
                             return `just now`;
-                        }else{
+                        } else {
                             return `${date.getMinutes() - mins} minute(s) ago`;
                         }
-                    }else{
+                    } else {
                         return `${date.getHours() - hours}hour(s) ago`;
                     }
-                }else{
-                    return `${date.getDate()- day} day(s) ago`
+                } else {
+                    return `${date.getDate() - day} day(s) ago`
                 }
-            }else{
+            } else {
                 return `${date.getMonth() - month} month(s) ago`
             }
-        }else{
+        } else {
             return `${date.getFullYear() - year} year(s) ago`
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         window.addEventListener("resize", toggleHeader);
 
-        return ()=>{
+        return () => {
             window.removeEventListener("resize", toggleHeader);
         }
     }, []);
 
-    function filterAll(){
+    function filterAll() {
         setCurrNotes(c => [...notes]);
     }
-    function filterFavs(){
-        setCurrNotes(c => notes.filter((note)=> note.fav ==true));
+    function filterFavs() {
+        setCurrNotes(c => notes.filter((note) => note.fav == true));
     }
 
     function favouriteNote(id) {
@@ -75,19 +75,19 @@ function Notes(){
         });
     }
 
-    function toggleHeader(){
-        if(editView.current.style.display == "flex"){
-            if(window.innerWidth>400){
+    function toggleHeader() {
+        if (editView.current.style.display == "flex") {
+            if (window.innerWidth > 400) {
                 headerRef.current.style.display = "flex";
-            }else{
-                if(isEditView == true){
+            } else {
+                if (isEditView == true) {
                     headerRef.current.style.display = "none";
                 }
             }
         }
     }
 
-    
+
 
     function toggleEditView(title = "", content = "") {
         if (!editView.current || !notesView.current || !headerRef.current) return;
@@ -95,14 +95,14 @@ function Notes(){
             // Hide edit, show notes
             setIsEditView(false);
             console.log("closing-edit-view");
-            if (headerRef.current){
+            if (headerRef.current) {
                 headerRef.current.style.display = "flex";
             }
         } else {
             // Show edit, hide notes
             setIsEditView(true);
             console.log("opening-edit-view");
-            if (headerRef.current && window.innerWidth <=400.5){
+            if (headerRef.current && window.innerWidth <= 400.5) {
                 headerRef.current.style.display = "none";
             }
         }
@@ -121,7 +121,7 @@ function Notes(){
         setNotes(n => {
             const newNotes = n.map(note =>
                 note.id === editingNoteId
-                    ? { ...note, title: editTitle, content: editContent , yearCreated : date.getFullYear(), monthCreated: date.getMonth(), dayCreated: date.getDate(), hourCreated: date.getHours(), minuteCreated: date.getMinutes()}
+                    ? { ...note, title: editTitle, content: editContent, yearCreated: date.getFullYear(), monthCreated: date.getMonth(), dayCreated: date.getDate(), hourCreated: date.getHours(), minuteCreated: date.getMinutes() }
                     : note
             );
             setCurrNotes(curr => filterNotes(newNotes, currFilter));
@@ -134,18 +134,29 @@ function Notes(){
         if (headerRef.current) headerRef.current.style.display = "flex";
     }
 
-    function filterNotes(notesList, filterword){
-        if(filterword =="favourites"){
+    function filterNotes(notesList, filterword) {
+        if (filterword == "favourites") {
             return notesList.filter(note => note.fav);
-        } else if(filterword == "All"){
+        } else if (filterword == "All") {
             return notesList;
-        } else{
-            return notesList.filter(note=> note.group === filterword);
+        } else {
+            return notesList.filter(note => note.group === filterword);
         }
     }
 
 
-    return(
+    function decideNoteSize(text) {
+        if (text.length >= 100){
+            return "note-lg note";
+        }
+        else if(text.length>= 50){
+            return "note-medium note";
+        } else{
+            return "note-small note";
+        }
+    }
+
+    return (
         <>
             <div
                 className="notes-view"
@@ -167,27 +178,27 @@ function Notes(){
                     </div>
                 </nav>
                 <div className="notes-grid">
-                    {currNotes.map((note)=>
-                        <div className="note" key={note.id} onClick={()=>editNote(note.title, note.content, note.id)}>
+                    {currNotes.map((note) =>
+                        <div className={decideNoteSize(note.content)} key={note.id} onClick={() => editNote(note.title, note.content, note.id)}>
                             <h1>{note.title}</h1>
                             <p>{note.content}</p>
                             <span>{getDateString(note.yearCreated, note.monthCreated, note.dayCreated, note.hourCreated, note.minuteCreated)}</span>
                             <div className="note-bottom">
-                                <span>{note.group ? note.group : "No Group" }</span>
+                                <span>{note.group ? note.group : "No Group"}</span>
                                 <div className="note-buttons">
                                     <figure>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>
                                     </figure>
                                     <figure className="fav-icon" onClick={e => { e.stopPropagation(); favouriteNote(note.id); }}>
-                                        {note.fav? 
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"></path></svg>
-                                        :
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412l7.332 7.332c.17.299.498.492.875.492a.99.99 0 0 0 .792-.409l7.415-7.415c2.354-2.354 2.354-6.049-.002-8.416a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595zm6.791 1.61c1.563 1.571 1.564 4.025.002 5.588L12 18.586l-6.793-6.793c-1.562-1.563-1.561-4.017-.002-5.584.76-.756 1.754-1.172 2.799-1.172s2.035.416 2.789 1.17l.5.5a.999.999 0 0 0 1.414 0l.5-.5c1.512-1.509 4.074-1.505 5.584-.002z"></path></svg>
+                                        {note.fav ?
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"></path></svg>
+                                            :
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412l7.332 7.332c.17.299.498.492.875.492a.99.99 0 0 0 .792-.409l7.415-7.415c2.354-2.354 2.354-6.049-.002-8.416a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595zm6.791 1.61c1.563 1.571 1.564 4.025.002 5.588L12 18.586l-6.793-6.793c-1.562-1.563-1.561-4.017-.002-5.584.76-.756 1.754-1.172 2.799-1.172s2.035.416 2.789 1.17l.5.5a.999.999 0 0 0 1.414 0l.5-.5c1.512-1.509 4.074-1.505 5.584-.002z"></path></svg>
                                         }
                                     </figure>
                                 </div>
                             </div>
-                            
+
                         </div>
                     )}
                 </div>
@@ -200,16 +211,16 @@ function Notes(){
                 ref={editView}
                 style={{ display: isEditView ? "flex" : "none" }}
             >
-                <button className="close-edit-view-btn" onClick={()=>toggleEditView("", "")}>
+                <button className="close-edit-view-btn" onClick={() => toggleEditView("", "")}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21 11H6.414l5.293-5.293-1.414-1.414L2.586 12l7.707 7.707 1.414-1.414L6.414 13H21z"></path></svg>
                 </button>
-                <input 
+                <input
                     type="text"
                     id="title-input"
                     placeholder="Enter a title..."
                     value={editTitle}
                     onChange={e => setEditTitle(e.target.value)}
-                    />
+                />
                 <textarea
                     id="content-txtarea"
                     placeholder="Your Note..."
