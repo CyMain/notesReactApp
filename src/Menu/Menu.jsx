94e285d2a1a2a42111778907dec0229865dddac0
CyMain <okikiolacyrus@gmail.com>
2025-07-02 17:12:01 +0100

    styles the add buttons

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -82,7 +82,7 @@ function Menu(){
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.842 6.218a1.977 1.977 0 0 0-.424-.628A1.99 1.99 0 0 0 20 5H8c-.297 0-.578.132-.769.359l-5 6c-.309.371-.309.91 0 1.281l5 6c.191.228.472.36.769.36h12a1.977 1.977 0 0 0 1.41-.582A1.99 1.99 0 0 0 22 17V7c0-.266-.052-.525-.158-.782z"></path></svg>
                         </figure>
                     </div>
-                    <button className="add-note-btn btn" onClick={createNoteAndEdit}>
+                    <button className="add-note-btn" onClick={createNoteAndEdit}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>
                     </button>
                     <div className="options-mobile-right">
